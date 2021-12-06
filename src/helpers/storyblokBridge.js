e457f73b9b0343c009f4c735f86bdfefd4adac83
Łukasz <lsliwaradioluz@gmail.com>
2021-12-06 13:27:36 +0100

    feat: asynchronous bridge loading

--- src/helpers/storyblokBridge.js
+++ src/helpers/storyblokBridge.js
@@ -1,18 +1,36 @@
+const createStoryblokBridge = (content, events, relations) => {
+  if (window) {
+    // eslint-disable-next-line
+    const instance = new StoryblokBridge({
+      resolveRelations: relations,
+    });
+    instance.on(events, (payload) => {
+      content.content = {
+        ...payload.story.content,
+        _meta: payload.story,
+      };
+    });
+  }
+};
+
 export const storyblokBridge = (
   content = { content: {} },
   events = ['change', 'input'],
   relations = [],
 ) => {
-  if (window) {
-    // eslint-disable-next-line
-    const instance = new StoryblokBridge({
-      resolveRelations: relations,
-    })
-    instance.on(events, (payload) => {
-      content.content = {
-        ...payload.story.content,
-        _meta: payload.story,
-      }
-    })
+  const isInsideStoryblokPreview = window.location.ancestorOrigins.contains('http://app.storyblok.com');
+  if (!isInsideStoryblokPreview) return
+
+  const existingScript = document.getElementById("storyblokBridge");
+  if (!existingScript) {
+    const script = document.createElement("script");
+    script.src = "//app.storyblok.com/f/storyblok-v2-latest.js";
+    script.id = "storyblokBridge";
+    document.body.appendChild(script);
+    script.onload = () => {
+      createStoryblokBridge(content, events, relations);
+    };
+  } else {
+      createStoryblokBridge(content, events, relations);
   }
 }
